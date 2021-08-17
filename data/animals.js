e1edd7f406dd780b7a3c16516c309781f79ee206
Triana Cerda <trianaRcerda@gmail.com>
2021-08-17 13:09:08 -0700

    added pics to animal data

--- data/animals.js
+++ data/animals.js
@@ -3,20 +3,24 @@ module.exports = [
     name: 'Tala',
     type_id: 1,
     snuggly: true,
+    image: '../assets/luna.png',
   },
   {
     name: 'Tony',
     type_id: 2,
     snuggly: true,
+    image:'../assets/tony.JPG',
   },
   {
     name: 'Bella',
     type_id: 2,
     snuggly: true,
+    image: '../assets/bella.JPG',
   },
   {
     name: 'Luna',
     type_id: 1,
     snuggly: true,
+    image: '../assets/tala.png',
   },
 ];
